60ae05baa5945dd6389225c0ef457fb7be3101d5
killinsun <noneck.0311@gmail.com>
2018-01-17 14:47:22 +0900

    Consider compatibility for PS version 2.0

---
+++ def1.ps1
@@ -0,0 +1,15 @@
+$def   = @{
+    dateCheck= @{ 
+        lastUpdate  = "2018/01/12" 
+        checkSkip   = $false
+    };
+    userCheck= @{
+        userAccount = "H1713022\z2h8636"
+        checkSkip   = $false
+    }
+    segmentCheck = @{
+        networkAddr = "10.120.248.0"
+        prefix      = 23
+        checkSkip   = $false
+    }
+}
